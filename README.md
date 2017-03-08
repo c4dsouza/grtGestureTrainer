3c339dbb5b19c2a6f0557377bab2b23df2e81743
Craig D'Souza <4cdsouza@gmail.com>
2017-03-08 12:47:33 -0500

    Added content to README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-grtGestureRecognitionTrainer
+grtGestureTrainer
+
+in src/main.cpp, modify NUM_DATASETS, NUM_GESTURES and NUM_DATASIZE to whatever you want, where
+--NUM_DATASETS is the number of datasets you want to use to train the model (1-16)
+--NUM_GESTURES is the number of gestures you want to use to train the model (1-11)
+--NUM_DATASIZE is ne number of rows of real data you want to use as a sample
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <sstream>
 
@@ -9,14 +10,22 @@ using namespace std;
 
 #define NUM_ROWS		8192
 #define NUM_INPUTS		192
+#define PROGRESS_BAR_W	100
+
+#define NUM_DATASETS    4
+#define NUM_GESTURES    2
+#define NUM_DATASIZE    256
+
 
 void loadDataFromFile(TimeSeriesClassificationData & mtrainingData, int dataSet, int gestureID) {
 	MatrixDouble timeseries;
 
     string filename = string("data/gestureData_") + to_string(dataSet) + string("_") + to_string(gestureID) + string(".csv");
     ifstream file(filename);
-    
-    for (int i = 0; i < NUM_ROWS; i++) {
+
+    printf("Loading data from: %s\n", filename.c_str());
+
+    for (int i = 1; i <= NUM_ROWS; i++) {
         VectorDouble sample(NUM_INPUTS);
         string line;
         
@@ -24,7 +33,7 @@ void loadDataFromFile(TimeSeriesClassificationData & mtrainingData, int dataSet,
         
         if ( !file.good() )
             exit(-1);
-    
+
         std::stringstream iss(line);
         
         for (int j = 0; j < NUM_INPUTS; j++) {
@@ -36,20 +45,36 @@ void loadDataFromFile(TimeSeriesClassificationData & mtrainingData, int dataSet,
         }
         
         timeseries.push_back( sample );
-        printf("%d\n", i);
+
+        if (i%82 == 0) {
+            printf("\r%3d%% [", i/82);
+
+            for (int j = 0; j < i/82; j++) {
+                printf("#");
+            }
+            for (int j = i/82; j < PROGRESS_BAR_W; j++) {
+                printf(" ");
+            }
+            printf("]");
+            fflush(stdout);
+        }
+
+        if (i%NUM_DATASIZE == 0) {
+            mtrainingData.addSample(gestureID, timeseries);
+            timeseries.clear();
+        }
     }
-    
-    mtrainingData.addSample(gestureID, timeseries);
-    timeseries.clear();
+
+    printf("\r\33[2K\033[ASuccessfully loaded: %s\n", filename.c_str());
 }
 
 
 int main (int argc, const char * argv[])
 {
-	printf("GRT Version: %s\n", GRTBase::getGRTVersion().c_str());
+	printf("GRT Version: %s\n\n", GRTBase::getGRTVersion().c_str());
 	
 	TimeSeriesClassificationData trainingData;
-	trainingData.setNumDimensions( 192 );
+	trainingData.setNumDimensions( NUM_INPUTS );
 
 	DTW dtw;
 	dtw.enableNullRejection( true );
@@ -57,9 +82,35 @@ int main (int argc, const char * argv[])
     dtw.setOffsetTimeseriesUsingFirstSample(true);
 
     GestureRecognitionPipeline pipeline;
-	pipeline.setClassifier( dtw );
+    pipeline.setClassifier( dtw );
 
-	loadDataFromFile(trainingData, 1, 1);
+    for (int i = 1; i <= NUM_DATASETS; i++) {
+        for (int j = 1; j <= NUM_GESTURES; j++) {
+            loadDataFromFile(trainingData, i, j);
+        }
+    }
+
+    if( trainingData.saveDatasetToFile("DTWTrainingData_Current.grt") ){
+        printf("Training data saved to file\n");
+    } else {
+        printf("WARNING: Failed to save training data to file\n");
+        return EXIT_FAILURE;
+    }
+
+
+    if ( pipeline.train( trainingData ) ) {
+        printf("Pipeline Trained!\n");
+    } else {
+        printf("WARNING: Failed to train pipeline\n");
+        return EXIT_FAILURE;
+    }
+
+    if( pipeline.save("DTWModel.grt") ){
+        printf("Classifier model saved to file\n");
+    } else {
+        printf("WARNING: Failed to save the classifier model!\n");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
